b85ac97aa8993fb24038e2968e4b6d6e8d03f911
jiqic <jiqic@nycstudents.net>
2025-09-12 13:47:17 +0000

    wasd

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   
+  System.out.println("Ji Qi Chen");
+  System.out.println("Ji Qi Chen Elmhurst");
+  System.out.println("Ji Qi Chen \nElmhurst");
+  }
+
+
+}
